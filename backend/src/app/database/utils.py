a49596f2c467980f51ed3b4ab6cd7bcfac4b2712
uriah rokach <uriahrokach>
2022-09-10 17:59:11 +0300

    Added helm chart and nessessary changes

--- backend/src/app/database/utils.py
+++ backend/src/app/database/utils.py
@@ -1,10 +1,14 @@
 from server.utils.config import get_config
 from .models import db
 
+import os
 
 def connect_to_db():
     """
     Connects to the mongodb database using the configuration connection string
     """
     conn_str = get_config().db.connection
-    db.connect(host=conn_str)
+    username = os.environ.get('MONGO_USERNAME')
+    password = os.environ.get('MONGO_PASSWORD')
+    
+    db.connect(username=username, password=password, host=conn_str)
